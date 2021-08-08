b20bbf46e1574afc1a209f6d0769d6d384978ef2
Jussefo91 <jussef.zamorano@spira.com.mx>
2021-08-07 23:04:31 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppBar from "./Components/AppBar";
+import {Container, Button} from "@material-ui/core";
+import { MakeStyles } from "@material-ui/core/styles";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar />
+      <Container maxWidth="sm">
+        <h2>APP</h2>
+        <Button variant="contained" color="primary">
+          Hola Mundo!
+        </Button>
+      </Container>
+    </>
   );
 }
 
