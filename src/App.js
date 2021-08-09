97bfa3152f4e0f3318f2cb3b42af1083c17d6123
Jussefo91 <jussef.zamorano@spira.com.mx>
2021-08-09 17:35:15 -0500

    Conexión a firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import "./App.css";
 import AppBar from "./Components/AppBar";
-import firebase from "firebase/app";
-import "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAXPCAkFI7QK34JyJeV85AnE3CRwxi-WQI",
-  authDomain: "transaccionapp.firebaseapp.com",
-  projectId: "transaccionapp",
-  storageBucket: "transaccionapp.appspot.com",
-  messagingSenderId: "171047776259",
-  appId: "1:171047776259:web:8a03a1eb50ecbcc6d19e36",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
--- src/Components/AppBar.js
+++ src/Components/AppBar.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -9,6 +9,25 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Icon from "@material-ui/core/Icon";
 import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import moment from "moment";
+import { render } from "@testing-library/react";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAXPCAkFI7QK34JyJeV85AnE3CRwxi-WQI",
+  authDomain: "transaccionapp.firebaseapp.com",
+  projectId: "transaccionapp",
+  storageBucket: "transaccionapp.appspot.com",
+  messagingSenderId: "171047776259",
+  appId: "1:171047776259:web:8a03a1eb50ecbcc6d19e36",
+};
+// Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig);
+const db = fb.firestore();
+const current_date = moment().format("DD/MM/YYYY");
+let dinero;
+let texto;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,16 +41,98 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ButtonAppBar() {
-const [state, setState] = useState({
-  valor: "",
-});
+function renderAhorro(doc) {
+  const totales = document.getElementById("totales1");
+  const ahorro = doc.data().total;
+  totales.innerHTML = ahorro;
+}
+function renderGastos(doc) {
+  const totales = document.getElementById("totales2");
+  const gastos = doc.data().total;
+  totales.innerHTML = gastos;
+}
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const dinero = e.target.valor.value;
-  
+const totalAhorro = () => {
+  db.collection("ahorro")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        renderAhorro(doc);
+      });
+    });
 };
+const totalGastos = () => {
+  db.collection("gastos")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        renderGastos(doc);
+      });
+    });
+};
+
+const actualiza = () => {
+  let cuanto = db
+    .collection("ahorro")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        let cuanto = doc.data().total;
+        cuanto = parseInt(cuanto) + parseInt(dinero);
+        db.collection("ahorro")
+          .doc("TuIFhhekmt1GCNx0GrwH")
+          .update({
+            total: cuanto,
+          })
+          .then((docRef) => {
+            console.log("Document update: ", cuanto);
+            totalAhorro();
+          })
+          .catch((error) => {
+            console.error("Error adding document: ", error);
+          });
+      });
+    });
+};
+
+export default function ButtonAppBar() {
+  const dataInit = {
+    Monto: "",
+    Fecha: "",
+    Descripcion: "",
+  };
+  const [data, setData] = useState(dataInit);
+  useEffect(() => {
+    totalAhorro();
+    totalGastos();
+  }, []);
+
+  const primero = (e) => {
+    e.preventDefault();
+    dinero = e.target.valor.value;
+    texto = e.target.descripcion.value;
+    if (texto === null || texto === "" || texto === undefined) {
+      texto = "Vacio";
+    }
+
+    setData({ ...data, Monto: dinero, Fecha: current_date, Descripcion: texto });
+
+    db.collection("pagos")
+      .add({
+        Monto: dinero,
+        Fecha: current_date,
+        Descripcion: texto,
+      })
+      .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+        actualiza();
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+    e.target.valor.value = "";
+    e.target.descripcion.value = "";
+  };
 
   const classes = useStyles();
 
@@ -52,12 +153,19 @@ const handleSubmit = (e) => {
       </AppBar>
       <br />
       <Container maxWidth="sm">
-        <form className={classes.root} noValidate autoComplete="on" onSubmit={handleSubmit}>
-          <TextField id="valor" label="Cuanto paga?" type="number" name="valor"/>
+        <form className={classes.root} noValidate autoComplete="on" onSubmit={primero}>
+          <TextField id="valor" label="Cuanto paga?" type="number" name="valor" />
+          <TextField id="descripcion" label="Descripcion" type="text" name="descripcion" />
           <Button variant="contained" color="primary" type="submit">
             Enter
           </Button>
         </form>
+        <p>Monto: {data.Monto}</p>
+        <p>Fecha: {data.Fecha}</p>
+        <p>Descripción: {data.Descripcion}</p>
+        <h4>Totales hasta ahora</h4>
+        <p id="totales1"></p>
+        <p id="totales2"></p>
       </Container>
     </div>
   );
